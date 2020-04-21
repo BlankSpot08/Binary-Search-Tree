40af07a6c1c4233531f5a8540083082cb9a21cc6
BlankSpot08 <arvinchu31@gmail.com>
2020-04-21 10:23:02 +0800

    Commit: 1.1

---
+++ src/processing/graphics/Functions.java
@@ -0,0 +1,11 @@
+package processing.graphics;
+
+public class Functions {
+    public void setOnAction() {
+        
+    }
+
+    public void setOnPressedKeyAction(char key, int keyCode) {
+
+    }
+}
